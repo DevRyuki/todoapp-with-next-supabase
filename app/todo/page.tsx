aa7b1308fffe6c496fabdb39d1d51583c6fe9437
RyukiSasaki <ryuki.sasaki@sasatech.co.jp>
2024-01-20 12:58:25 +0900

    commit

---
+++ app/todo/page.tsx
@@ -0,0 +1,46 @@
+import { cookies } from "next/headers"
+import { createClient } from "@/utils/supabase/server"
+import {revalidatePath} from "next/cache";
+
+export default async function Page() {
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore)
+
+    const { data } = await supabase.from('todo').select('*').order('created_at', { ascending: false })
+
+  // data : any[] | null
+    if (!data) return (<div>データがありません。</div>)
+
+  async function action(formData: FormData) {
+    'use server'
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore)
+
+    //formの入力を取得
+    const values = {
+      name: formData.get('name') as string,
+    }
+
+    const {error} = await supabase.from("todo").insert(values)
+
+    if (error) throw new Error("Error:" + JSON.stringify(error))
+    revalidatePath('/todo')
+  }
+
+    return (
+      <div>
+          <form action={action}>
+            <input className={"border"} type="text" name="name" id="name" />
+            <button className={"border"} type="submit">Submit</button>
+          </form>
+
+          {
+              data.map((todo) => (
+              <div key={todo.id}>
+                  <p>{todo.name}</p>
+              </div>
+          ))}
+      </div>
+
+    )
+}
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,5 +1,6 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import {Database} from "@/types/database.types"
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient(
