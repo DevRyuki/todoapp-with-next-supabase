0c262701529fbcfdcf0b938065a5618a0649886a
RyukiSasaki <ryuki.sasaki@sasatech.co.jp>
2024-02-03 14:19:39 +0900

    Delete追加

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers"
 import { createClient } from "@/utils/supabase/server"
 import {revalidatePath} from "next/cache";
+import {id} from "postcss-selector-parser";
 
 export default async function Page() {
     const cookieStore = cookies();
@@ -24,22 +25,40 @@ export default async function Page() {
     const {error} = await supabase.from("todo").insert(values)
 
     if (error) throw new Error("Error:" + JSON.stringify(error))
+
     revalidatePath('/todo')
   }
 
+    async function deleteActionHandler(formData: FormData) {
+      "use server"
+
+      const cookieStore = cookies();
+      const supabase = createClient(cookieStore)
+
+      const {error} = await supabase.from("todo").delete().eq("id", formData.get("id"))
+
+      if (error) throw new Error("Error:" + JSON.stringify(error))
+
+      revalidatePath('/todo')
+
+    }
+
     return (
-      <div>
+      <div className={"space-y-2"}>
           <form action={actionHandler}>
             <input className={"border"} type="text" name="name" id="name" />
             <button className={"border"} type="submit">Submit</button>
           </form>
-
           {
               data.map((todo) => (
-              <div key={todo.id}>
+                <div key={todo.id} className={"flex gap-2 items-center"}>
+                  <form action={deleteActionHandler}>
+                    <input type={"text"} name={"id"} defaultValue={todo.id} readOnly className={"hidden"}/>
+                    <button className={"border p-2"} type={"submit"}>X</button>
+                  </form>
                   <p>{todo.name}</p>
-              </div>
-          ))}
+                </div>
+              ))}
       </div>
     )
 }
